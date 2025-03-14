7b3a63d330e06a3e08418e56e74e1b9b4503964a
terridb <terrideboer99@gmail.com>
2025-03-14 13:27:20 +0100

    Deel 2, opdracht 2 afgerond

--- src/pages/postoverview/PostOverview.jsx
+++ src/pages/postoverview/PostOverview.jsx
@@ -1,17 +1,45 @@
-import posts from "../../constants/data.json"
 import PostPreview from "../../components/postPreview/PostPreview.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function PostOverview() {
+    const [posts, setPosts] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const getAllPosts = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get("http://localhost:3000/posts");
+            setPosts(response.data);
+        } catch (err) {
+            setError(err);
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getAllPosts();
+    }, []);
+
     return (
         <>
             <h1>Bekijk alle {posts.length} posts op het platform</h1>
             <div className="post-preview-container">
-                {posts.map(post => (
-                    <PostPreview
-                        key={post.id}
-                        post={post}
-                    />
-                ))}
+                {loading && <span className="loader"/>}
+                {error && <div className="error-message">Error: {error}</div>}
+                {posts && posts.length > 0 ? (
+                    posts.map(post => (
+                        <PostPreview
+                            key={post.id}
+                            post={post}
+                        />
+                    ))
+                ) : (
+                    <p>Er zijn geen posts beschikbaar</p>
+                )}
             </div>
         </>
     );
